5591456494acaa1530c17076ce87783dc42cc0aa
Harsh Vardhan <harsh.1vardhan123@gmail.com>
2024-01-21 00:32:19 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,38 +6,34 @@ function App() {
 
   return (
     <>
-     <div className='flex items-center justify-center h-screen'>
-        <div
-          className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
-          style={{
-              backgroundImage: `url(https://images.pexels.com/photos/3011842/pexels-photo-3011842.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`,
-          }}
-        >
-          <div className='flex items-center justify-center bg-black'>
+     
+          <div id='container'>
             <h1>Password Generator</h1>
-            <div>
+            <div class='input-button'>
               <input 
+                id='input'
                 type="text" 
                 placeholder='Password' 
-                readOnly 
+                
               />
-              <button>
+              <button id='copy'>
                 Copy
               </button>
-            </div>
-            <div>
+              </div>
+            <div id='range-length'>
               <input 
+                id='range'
                 type="range"
                 min={5}
                 max={20}
                 
 
               />
-              <label>
+              <label id='length'>
                 Length:
               </label>
-            </div>
-            <div>
+              </div>
+              <div>
               <input 
                 type="checkbox" 
               />
@@ -50,13 +46,13 @@ function App() {
               <label>
                 Numbers
               </label>
-            </div>
+              </div>
 
 
           </div>
+          
 
-        </div>
-     </div>
+        
     </>
   )
 }
